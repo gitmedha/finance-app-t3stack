dc0850af4463370a3e656ffdb7b8abc03f132dc0
GNANAM_RAJAGOPALAN <gnanam.rajagopalan@medha.org.in>
2025-07-21 18:17:17 +0530

    fix type erros

--- src/app/budget/filter.tsx
+++ src/app/budget/filter.tsx
@@ -143,7 +143,7 @@ const BudgetFilterForm: React.FC<BudgetFilterFormProps> = ({ filters, handleSele
               </DropdownMenu.Content>
             </DropdownMenu.Root>
           </div>
-          {/* Department */}
+          {/* Department Dropdown */}
           <div className="w-full md:w-56 flex flex-col gap-1">
             <label htmlFor="department-dropdown" className="text-md font-medium text-gray-700">
               Select Department :
